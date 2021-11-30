b845650b68f15fbf53e21f7754137ca37034bbab
Max Z <maxsis100@gmail.com>
2021-11-30 10:40:31 +0800

    added all these

---
+++ Pomodoro/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pomodoro
+//
+//  Created by Max Z on 30/11/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
