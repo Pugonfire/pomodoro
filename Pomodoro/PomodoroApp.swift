23b4f35c1caffa75aa947f3e39df12f7ca33528f
Max Z <maxsis100@gmail.com>
2021-11-30 13:01:28 +0800

    Finish UI

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 @main
-struct pomodoroApp: App {
+struct MacMenuApp: App {
+    // Connect AppDelegate
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+// Build Menu Button and Pop Over Menu...
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    // Status Bar Item
+    var statusItem: NSStatusItem?
+    // Popover
+    var popOver = NSPopover()
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let menuView = MenuView()
+        
+        // Create Popover
+        popOver.behavior = .transient
+        popOver.animates = true
+        // Setting empty view controller
+        // and setting view as SwiftUI view
+        // with the help of hosting controller
+        popOver.contentViewController = NSViewController()
+        popOver.contentViewController?.view = NSHostingView(rootView: menuView)
+        
+        // Creating status bar button
+    }
+}
+
