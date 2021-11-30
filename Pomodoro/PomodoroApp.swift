26c3759a84387ba177e50a40d8e797ec4d240298
Max Z <maxsis100@gmail.com>
2021-11-30 10:57:23 +0800

    Remake swiftui

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Max Z on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
